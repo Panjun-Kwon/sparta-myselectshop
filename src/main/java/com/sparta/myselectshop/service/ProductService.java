3b60c31c7ce20ffadd080145b0760b4154e1694d
Panjun-Kwon <pndlm1979@naver.com>
2024-07-29 13:33:31 +0900

    관심 상품 등록 API

---
+++ src/main/java/com/sparta/myselectshop/service/ProductService.java
@@ -0,0 +1,22 @@
+package com.sparta.myselectshop.service;
+
+import com.sparta.myselectshop.dto.*;
+import com.sparta.myselectshop.entity.*;
+import com.sparta.myselectshop.repository.*;
+import lombok.*;
+import org.springframework.stereotype.*;
+import org.springframework.transaction.annotation.*;
+
+@Service
+@RequiredArgsConstructor
+public class ProductService {
+
+    private final ProductRepository productRepository;
+    public static final int MIN_MY_PRICE = 100;
+
+    public ProductResponseDto createProduct(ProductRequestDto requestDto) {
+        Product product = productRepository.save(new Product(requestDto));
+        return new ProductResponseDto(product);
+    }
+
+}
