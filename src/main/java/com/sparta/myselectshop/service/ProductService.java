232d3788ef97b4558f3b3c7d8a23dec9ac7f4994
Panjun-Kwon <pndlm1979@naver.com>
2024-07-29 13:52:35 +0900

    Scheduler 기능 구현

--- src/main/java/com/sparta/myselectshop/service/ProductService.java
+++ src/main/java/com/sparta/myselectshop/service/ProductService.java
@@ -2,6 +2,7 @@ package com.sparta.myselectshop.service;
 
 import com.sparta.myselectshop.dto.*;
 import com.sparta.myselectshop.entity.*;
+import com.sparta.myselectshop.naver.dto.*;
 import com.sparta.myselectshop.repository.*;
 import lombok.*;
 import org.springframework.stereotype.*;
@@ -45,5 +46,13 @@ public class ProductService {
         }
 
         return responseDtoList;
+    }
+
+    @Transactional
+    public void updateBySearch(Long id, ItemDto itemDto) {
+        Product product = productRepository.findById(id).orElseThrow(() ->
+                new NullPointerException("해당 상품은 존재하지 않습니다."));
+
+        product.updateByItemDto(itemDto);
     }
 }
