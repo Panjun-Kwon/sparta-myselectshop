12db64e7eb134d07a8067c82d3b6955d864b0bca
Panjun-Kwon <pndlm1979@naver.com>
2024-07-29 17:04:29 +0900

    폴더별 관심 상품 조회 구현

--- src/main/java/com/sparta/myselectshop/repository/ProductRepository.java
+++ src/main/java/com/sparta/myselectshop/repository/ProductRepository.java
@@ -1,5 +1,6 @@
 package com.sparta.myselectshop.repository;
 
+import com.sparta.myselectshop.dto.*;
 import com.sparta.myselectshop.entity.*;
 import org.springframework.data.domain.*;
 import org.springframework.data.jpa.repository.*;
@@ -8,4 +9,6 @@ import java.util.*;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
     Page<Product> findAllByUser(User user, Pageable pageable);
+
+    Page<Product> findAllByUserAndProductFolderList_FolderId(User user, Long folderId, Pageable pageable);
 }
